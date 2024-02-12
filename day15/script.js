abc5c59e09e7be512f5581fdb5f5f025924c4d2f
kakanics <affan.bb7@gmail.com>
2024-02-12 21:57:02 +0500

    day 15 - clock

---
+++ day15/script.js
@@ -0,0 +1,8 @@
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById('time').textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
